73e1cefe7cb5340bd0559e87f5e8ddf8ba2fdb27
Chris Aquino <chris@radishmouse.com>
2021-01-25 14:36:21 -0500

    refactors the form field into a reusable component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 
 import { useState } from 'react';
+import ColorPicker from './ColorPicker';
 
 function App() {
-  const [red, setRed] = useState(23); // red
-  const [green, setGreen] = useState(10); // green
-  const [blue, setBlue] = useState(9); // blue
+  const [red, setRed] = useState(255); // red
+  const [green, setGreen] = useState(255); // green
+  const [blue, setBlue] = useState(255); // blue
 
   // fakey-changey the "body" (but really the App component)
+  // CSS-in-JS
   const appStyle = {
     backgroundColor: `rgb(${red}, ${green}, ${blue})`
   };
@@ -16,37 +18,25 @@ function App() {
   return (
     <div className="fakey-body" style={appStyle}>
 
-      R: <input type="range" min="0" max="255" 
-
-        value={red} 
-        onChange={(event) => {
-          console.log(event.target.value);
-          setRed(event.target.value);
-        }}
-
+      <ColorPicker
+        label="Red"
+        color={red}
+        setColor={setRed}
       />
-      {red}
+
       <br />
-      G: <input type="range" min="0" max="255"
-
-        value={green} 
-        onChange={(event) => {
-          console.log(event.target.value);
-          setGreen(event.target.value);
-        }}
-
+      <ColorPicker 
+        label="Green"
+        color={green}
+        setColor={setGreen}
       />
-      {green}
+
       <br />
-      B: <input type="range" min="0" max="255"
-        value={blue} 
-        onChange={(event) => {
-          console.log(event.target.value);
-          setBlue(event.target.value);
-        }}      
-      />
-      {blue}
-      
+      <ColorPicker 
+        label="Blue"
+        color={blue}
+        setColor={setBlue}
+      />      
     </div>
   );
 }
